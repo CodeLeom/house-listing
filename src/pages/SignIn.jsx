30b487a8a162b8f3f41ff4b94fce94b13ae16e4d
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-02-14 00:39:42 +0100

    designing the sign-in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,43 @@
-
+import { useState } from "react"
+import { NavLink, Navigate, useNavigate } from "react-router-dom"
+import {FaArrowRight} from "react-icons/fa"
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 function SignIn() {
+  const [showPass, setShowPass] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const {email, password} = formData
+
+  const navigate = useNavigate()
+
+  const onChange = () => {
+
+  }
+
   return (
     <div className="mt-5 mb-5">
-      SignIn
+        <div>
+          <header className="fw-bold mb-3">
+            Welcome Back
+          </header>
+
+          <main>
+            <form>
+              <input type="email" className="emailInput" placeholder="enter your email" id="email" value={email} onChange={onChange} />
+
+              <div className="passwordInputDiv">
+                <input type={showPass ? 'text' : 'password'} className="passwordInput" placeholder="input your password" id="password" value={password} onChange={onChange} />
+                <img src={visibilityIcon} alt="show password" className="showPassword" onClick={() => setShowPass((prevState) => !prevState)} />
+              </div>
+
+              <NavLink to='/forgot-password' className="forgotPasswordLink">Forgot Password</NavLink>
+            </form>
+          </main>
+        </div>
     </div>
   )
 }
