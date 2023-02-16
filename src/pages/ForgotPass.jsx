d5937fabc42083bb21c858e063b583f15b5feeee
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-02-16 07:14:30 +0100

    implemented forgot password function

--- src/pages/ForgotPass.jsx
+++ src/pages/ForgotPass.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth'
+import {toast} from 'react-toastify'
+import {FaArrowRight} from "react-icons/fa"
 
 
 function ForgotPass() {
+  const [email, setEmail] = useState('')
+  const onChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Reset Email sent successfully!")
+
+    } catch (err) {
+      toast.error('Could not send reset email')
+    }
+  }
+
   return (
-    <div>
-      Forgot Password
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input type="email" className="emailInput" placeholder="enter your email" id="email" value={email} onChange={onChange} />
+
+          <Link className="forgotPasswordLink" to='/signin'>Sign In</Link>
+
+          <div className="signInBar">
+            <div className="signInText">
+              Send Reset Link
+            </div>
+            <button className="signInButton">
+            <FaArrowRight fill="#fff" width={34} height={34} />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   )
 }
