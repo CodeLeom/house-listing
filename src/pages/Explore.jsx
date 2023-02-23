3b70568283298b814eee6f3c266d4087e994bd63
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-02-23 17:43:59 +0100

    explore page category feature

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,34 @@
+import {Link} from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 
 function Explore() {
   return (
-    <div>
-      Home
+    <div className='explore'>
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* slider */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+            <img src={rentCategoryImage} alt='rent image' className='exploreCategoryImg' />
+            <p className='exploreCategoryName'>
+            Places For Rent
+          </p>
+          </Link>
+          <Link to='/category/sell'>
+            <img src={sellCategoryImage} alt='sell image' className='exploreCategoryImg' />
+            <p className='exploreCategoryName'>
+            Places For Sale
+          </p>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
