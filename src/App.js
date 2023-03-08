3521dd23a0e09c92eefbeb132299094e27b561f7
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-03-08 13:06:56 +0100

    created the single listing page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import SignUp from './pages/SignUp';
 import ForgotPass from './pages/ForgotPass';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
-
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 
 function App() {
@@ -33,6 +34,8 @@ function App() {
                   <Route path='/signup' element={<SignUp />} />
                   <Route path='/forgot-password' element={<ForgotPass />} />
                   <Route path='/create-listing' element={<CreateListing />} />
+                  <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+                  <Route path='/contact/:ownerId' element={<Contact />} />
                 </Routes>
               </main>
             <Footer />
